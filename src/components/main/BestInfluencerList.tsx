a1a5d31b3601e565fb410fb8b67f0f02617a5bcb
USERMIC-Q8MSLVD\Administrator <sssb2046@gmail.com>
2024-07-21 22:13:40 +0900

    style: main 디자인 추가

--- src/components/main/BestInfluencerList.tsx
+++ src/components/main/BestInfluencerList.tsx
@@ -6,7 +6,7 @@ function BestInfluencerList() {
       <h2 className="font-bold my-3">현재 인기 인플루언서</h2>
       <div className="w-full h-[400px] overflow-y-auto">
         <div className="border rounded-md bg-gray-300 w-full min-h-[100px] mx-auto p-5 grid grid-rows-1 grid-flow-col mb-4">
-          <div className="bg-gray-100 rounded-md w-[80px] h-[80px]"></div>
+          <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
           <div>
             <p className="mb-2">인싸우르르</p>
             <p className="mb-2">#OOO</p>
--- src/components/main/SubInfluencer.tsx
+++ src/components/main/SubInfluencer.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
 
 function SubInfluencer() {
   return (
-    <div className='border w-[70%] h-[200px] mx-auto'>SubInfluencer</div>
-  )
+    <>
+      <div className="border w-[70%] h-[200px] mx-auto p-5">
+        <h2 className="font-bold my-3">내가 구독한 인플루언서</h2>
+        <div className="overflow-x-auto flex gap-10">
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+          <div className="gird w-[80px] text-center">
+            <div className="bg-gray-100 rounded-md w-[80px] h-[80px]">이미지</div>
+            <p>우르르</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default SubInfluencer
+export default SubInfluencer;

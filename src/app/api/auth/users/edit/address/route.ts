0a603c5de2b2e1e0f4d2bbd19c1129529941e8a2
lyuna29 <hyunah0418@gmail.com>
2024-07-29 23:20:10 +0900

    fix

--- src/app/api/auth/users/edit/address/route.ts
+++ src/app/api/auth/users/edit/address/route.ts
@@ -1,27 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
-  try {
-    const keyword = req.nextUrl.searchParams.get("keyword");
-    const currentPage = req.nextUrl.searchParams.get("currentPage");
+  const keyword = req.nextUrl.searchParams.get("keyword");
+  const currentPage = req.nextUrl.searchParams.get("currentPage");
 
-    const res = await fetch(
-      `https://business.juso.go.kr/addrlink/addrLinkApi.do?countPerPage=8&currentPage=${currentPage}&keyword=${keyword}&confmKey=${process
-        .env.NEXT_PUBLIC_ADDRESS_API_KEY!}&resultType=json`
-    );
+  const res = await fetch(
+    `https://business.juso.go.kr/addrlink/addrLinkApi.do?countPerPage=8&currentPage=${currentPage}&keyword=${keyword}&confmKey=${process
+      .env.NEXT_PUBLIC_ADDRESS_API_KEY!}&resultType=json`
+  );
 
-    if (!res.ok) {
-      const errorData = await res.json();
-      console.error("Server response error:", errorData);
-      return NextResponse.json(errorData, { status: 500 });
-      // throw new Error(`HTTP ERROR OCCURED. status: ${res.status}`);
-    }
-
-    const data = await res.json();
-
-    return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(error, { status: 500 });
-    console.log(error);
+  if (!res.ok) {
+    const errorData = await res.json();
+    console.error("Server response error:", errorData);
+    /* throw new Error(`HTTP ERROR OCCURED. status: ${res.status}`); */
+    return NextResponse.json(errorData, { status: 500 });
   }
+
+  const data = await res.json();
+
+  return NextResponse.json(data, { status: 200 });
 };

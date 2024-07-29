093bf02075fdf0c3e0c437c6ec0ab3c281afc15b
mini-woong <mini_0@inu.ac.kr>
2024-07-29 13:43:13 +0900

    update: 메세지 불러옴

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,21 +1,39 @@
 "use client";
-import { useUserData } from "@/hooks/useUserData";
-import { getMessages } from "@/services/chats/chats.service";
 
-export default function Chat () {
-  const { data } = useUserData();
-  console.log(data);
+import { useUserData } from "@/hooks/useUserData";
+import { createClient } from "../../../supabase/client";
+
+
+export default function Chat() {
+  const userdata = useUserData().data;
+  console.log(userdata);
+  const supabase = createClient();
 
   const createChatRoom = () => {
-
+    //유저의 대화 시작하기
   }
   const enterChatChannel = () => {
-
+    //유저의 대화방 입장하기
   }
   const getChatMessages = async () => {
-    const user_id = "b64dbaca-2997-46b6-a4b2-d9c6e6fce069"
-    const channel_id = "1"
-    await getMessages();
+    const user_id = userdata.id
+    if (userdata.approve) {
+      const { data, error } = await supabase
+        .from('chat_messages')
+        .select('*')
+        .eq('channel_id', 1)
+      console.log(data)
+    } else {
+      const influ_id = 'e717cc1d-16de-43c6-a90b-a567022b48e0'
+      const { data, error } = await supabase
+        .from('chat_messages')
+        .select('*')
+        .in('user_id', [user_id, influ_id])
+        .eq('channel_id', 1)
+      console.log(data)
+    }
+
+
   }
   const sendChatMessage = () => {
 
@@ -25,7 +43,7 @@ export default function Chat () {
   }
   return (
     <>
-     <button onClick={getChatMessages}>채팅방 입장하기</button>
+      <button onClick={getChatMessages}>채팅방 입장하기</button>
     </>
   );
 }

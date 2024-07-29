762533299db1c63ce2429dcb648b96a0f16a0bca
parksumi <parksumi0127@gmail.com>
2024-07-30 00:28:38 +0900

    fix: 오류 수정 중 코드 수정

--- supabase/client.ts
+++ supabase/client.ts
@@ -1,11 +1,5 @@
-import { Database } from '../types/supabase';
-import { createBrowserClient } from '@supabase/ssr';
-
-// const SUPABASE_PROJECT_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-
-// const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
-
-// const supabase = createClient<Database>(SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY);
+import { Database } from "../types/supabase";
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient<Database>(

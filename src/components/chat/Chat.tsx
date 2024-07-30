f89a921f1bfd47dd3c1d49e95ed345f4eec94b2d
mini-woong <mini_0@inu.ac.kr>
2024-07-30 22:22:31 +0900

    update: 유저 채팅 보내기

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,11 +2,15 @@
 
 import { useUserData } from "@/hooks/useUserData";
 import { createClient } from "../../../supabase/client";
+import { useRef, useState } from "react";
 
 export default function Chat() {
   const userdata = useUserData().data;
   const supabase = createClient();
 
+  //const [content, setContent] = useState<{ message : string } | null>(null)
+  const [message, setMessage] = useState<String>('');
+  const textRef = useRef<HTMLInputElement>(null);
   const createChatRoom = () => {
     //유저의 대화 시작하기
   }
@@ -34,20 +38,46 @@ export default function Chat() {
       console.log(data)
     }
   }
-  
-  const sendChatMessage = () => {
 
+  const handleTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value)
   }
+
+  const sendChatMessage = async () => {
+    const user_id = userdata.id
+    const channel_id = 1;
+    //유저가 해당 채팅방을 구독하고 있는지 확인하는 함수 필요
+    console.log(message)
+    const content = JSON.stringify({
+      message : message
+    });
+
+    const { data, error } = await supabase
+      .from('chat_messages')
+      .insert({
+        channel_id: channel_id,
+        content:  JSON.parse(content),
+        user_id: user_id
+      })
+      if (error) {
+        console.log('채팅 보내기 실패')
+        console.log(error)
+      }
+  }
+  
   const receiveChatMessage = () => {
 
   }
 
-  
+
 
   return (
     <>
-      
-      <button onClick={getChatMessages}>채팅방 입장하기</button>
+      <textarea className="border-style: solid; border-color: rgb(0 0 0);" onChange={handleTextarea}></textarea>
+      <button className="border-style: solid; border-color: rgb(0 0 0);" onClick={(message!='') ? sendChatMessage : () => {
+        console.log('보낼 내용 없음')
+      }}>채팅 보내기</button>
+      <button className="border-style: solid; border-color: rgb(0 0 0);" onClick={getChatMessages}>채팅방 입장하기</button>
     </>
   );
 }

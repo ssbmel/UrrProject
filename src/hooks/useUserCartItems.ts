ca65a77dfee40789c4e058248177f5d7eecdf9c4
parksumi <parksumi0127@gmail.com>
2024-08-02 20:29:42 +0900

    fix: 장바구니 기능 수정

--- src/hooks/useUserCartItems.ts
+++ src/hooks/useUserCartItems.ts
@@ -3,7 +3,13 @@
 import { useQuery } from "@tanstack/react-query";
 
 const userCartList = async (userId: string) => {
-  const response = await fetch(`/api/auth/cart?userId=${userId}`);
+  const response = await fetch("/api/auth/cart", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ user_id: userId })
+  });
   const data = response.json();
   return data;
 };

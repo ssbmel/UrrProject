a7451f5262dbaa06e583c43d19c42ea86c3a1564
ssbmel <sssb2046@gmail.com>
2024-08-06 23:24:16 +0900

    style: 인플루언서 목록 페이지 css 수정

--- src/components/influencer/InfluencerList.tsx
+++ src/components/influencer/InfluencerList.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 import defaultImg from "../../../public/images/default.png";
 import emptyImg from "../../../public/bgImg/emptyImg.png";
 import { User } from "../../../types/common";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
+import EmptyHeartIcon from "../../../public/icon/emptyheart.svg";
+import FullHeartIcon from "../../../public/icon/fullheart.svg";
 
 function InfluencerList() {
+  const [isLiked, setIsLiked] = useState(false);
   const getUserData = async () => {
     try {
       const response = await fetch("/api/auth/users/infuser/allinfuser");
@@ -31,31 +34,49 @@ function InfluencerList() {
     getUserData();
   }, []);
 
+  const handleHeartChange = () => {
+    setIsLiked((prevIsLiked) => !prevIsLiked);
+  };
+
   return (
-    <div className="w-full h-svh p-5">
-      <div className="w-full min-h-[250px] h-[30%] p-2">
+    <div className="w-full bg-[#F4F4F4] mx-auto">
+      <div className="w-full min-h-[250px] h-[30%] p-4 bg-[#FFFFFE]">
         <h1 className="font-bold text-lg">내가 구독중인 인플루언서</h1>
         <div className="flex overflow-x-auto mt-5">
           <div className="flex flex-col items-center mx-auto">
-            <Image src={emptyImg} alt="empty" width={130} className="mx-auto my-5"></Image>
-            <p className="text-[#4C4F52] text-[16px] mx-auto">현재 구독중인 인플루언서가 없습니다.</p>
+            <div className="relative w-[150px] h-[100px] my-3">
+              <Image src={emptyImg} alt="empty" fill sizes="100px" className="mx-auto my-5 object-cover" />
+            </div>
+            <p className="text-[#4C4F52] text-[16px] my-5">현재 구독중인 인플루언서가 없습니다.</p>
           </div>
         </div>
       </div>
-      <hr />
-      <div className="w-full h-[70%] p-2 my-5">
-        <h1 className="font-bold text-lg mb-3">전체 인플루언서</h1>
-        <div className="w-full gap-3 grid grid-cols-3 auto-rows-max overflow-y-auto scrollbar mx-auto">
+      <div className="w-full h-[70%] p-4 my-5 bg-[#FFFFFE]">
+        <h1 className="font-bold text-lg mb-3">인플루언서</h1>
+        <div className="w-full gap-1 grid grid-cols-3 auto-rows-max overflow-y-auto scrollbar mx-auto">
           {infUser?.map((inf) => (
             <div key={inf.id} className="flex flex-col items-center justify-center w-[100px] text-center mx-auto">
-              <div className="relative w-[100px] h-[100px] mb-2">
+              <div className="relative w-[120px] h-[120px] mb-2">
                 <Image
                   src={inf.profile_url || defaultImg}
                   alt="img"
                   fill
-                  sizes="100px"
+                  sizes="120px"
                   className="rounded-md object-cover gradient-border"
                 />
+                {isLiked ? (
+                  <div className="absolute bottom-1 right-2">
+                    <button onClick={handleHeartChange}>
+                      <FullHeartIcon />
+                    </button>
+                  </div>
+                ) : (
+                  <div className="absolute bottom-1 right-2">
+                    <button onClick={handleHeartChange}>
+                      <EmptyHeartIcon />
+                    </button>
+                  </div>
+                )}
               </div>
               <p className="text-sm">{inf.nickname}</p>
             </div>

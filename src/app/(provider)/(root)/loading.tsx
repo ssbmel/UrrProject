8aeca321c694a8bd5cfa3e62594455eeac0d7a2c
lyuna29 <hyunah0418@gmail.com>
2024-08-06 20:08:18 +0900

    update:loading, fail, 결제바로취소

---
+++ src/app/(provider)/(root)/loading.tsx
@@ -0,0 +1,12 @@
+import LoadingUrr from "@/components/common/loading/LoadingUrr";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <LoadingUrr />
+    </div>
+  );
+};
+
+export default Loading;
--- src/components/payment/Fail.tsx
+++ src/components/payment/Fail.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import fail from "../../../public/images/orderFail.png";
 
 export default function Fail() {
   const router = useRouter();
@@ -13,23 +15,28 @@ export default function Fail() {
     setQueryParams({ code, message });
   }, []);
 
-  const { code, message } = queryParams;
+  const { message } = queryParams;
 
   // if (!code || !message) {
   //   return <div className="flex justify-center items-center min-h-screen bg-gray-100">로딩 중...</div>;
   // }
 
   return (
-    <div className="flex justify-center items-center w-full h-screen ">
+    <div className="flex flex-col justify-center items-center w-full h-screen ">
+      <div className="relative w-[96px] h-[142px]">
+        <Image src={fail} alt="결제실패 이미지" fill sizes="96px" className=" object-cover" />
+      </div>
       <div className="bg-white p-8 rounded-lg text-center mb-[80px]">
-        <h2 className="text-red-500 text-2xl font-bold mb-4">결제에 실패하셨습니다</h2>
-        <p className="text-gray-800 text-lg mb-2">
-          <span className="font-semibold">에러 코드:</span> {code}
-        </p>
-        <p className="text-gray-800 text-lg">
-          <span className="font-semibold">실패 사유:</span> {message}
-        </p>
+        <h2 className="text-red-500 text-2xl font-bold m-2">주문을 완료하지 못했어요</h2>
+        <p className="text-[18px] text-[#4C4F52]">이용에 불편을 드려 죄송합니다</p>
+
         <Link href={"/products/list"}>
+          <div className="w-[250px] h-[74px] border border-[#F2F2F2] rounded-md">
+            <p className="text-gray-800 text-lg">
+              <span className="font-semibold text-sm">실패 사유</span>
+              <span>{message}</span>
+            </p>
+          </div>
           <button className="w-44 h-8 mt-2 rounded-md text-white bg-[#1A82FF]">다시 구매하러 가기</button>
         </Link>
       </div>

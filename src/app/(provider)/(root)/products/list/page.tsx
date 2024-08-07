7ec31867d5b926eddcfd606860f036c21c4ac4f0
lyuna29 <hyunah0418@gmail.com>
2024-08-07 12:27:49 +0900

    update: 카테고리화, 상품 더보기 버튼

--- src/app/(provider)/(root)/products/list/page.tsx
+++ src/app/(provider)/(root)/products/list/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import ListCategory from "@/components/products/list/ListCategory";
 import ProductsList from "@/components/products/list/ProductsList";
+import { useState } from "react";
 
 const list = () => {
+  const [selectedCategory, setSelectedCategory] = useState("전체");
   return (
     <div>
-      <ListCategory />
-      <ProductsList />
+      <ListCategory onSelectCategory={setSelectedCategory} />
+      <ProductsList selectedCategory={selectedCategory} />
     </div>
   );
 };

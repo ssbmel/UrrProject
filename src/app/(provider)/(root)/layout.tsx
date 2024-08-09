7764267545d4a63059fef44b679c944358d49963
lyuna29 <hyunah0418@gmail.com>
2024-08-09 14:05:18 +0900

    fix:layout url 이미지 추가

--- src/app/(provider)/(root)/layout.tsx
+++ src/app/(provider)/(root)/layout.tsx
@@ -2,7 +2,14 @@ import BottomNav from "@/components/common/bottomnav/BottomNav";
 import UpButton from "@/components/common/button/UpButton";
 import Footer from "@/components/common/footer/Footer";
 import Header from "@/components/common/header/Header";
+
 import React, { PropsWithChildren } from "react";
+
+export const metadata = {
+  title: "URR(우르르)",
+  description: "인플루언서와 소통하고 공구상품을 구매해보세요!",
+  ogImage: "/main.png"
+};
 
 const Mainlayout = ({ children }: PropsWithChildren) => {
   return (

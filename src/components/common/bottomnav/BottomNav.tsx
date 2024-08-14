42843c214c0f9db062a52da9421d90b53a3b9531
parksumi <parksumi0127@gmail.com>
2024-08-14 21:17:19 +0900

    style: 하단네비 수정 중

--- src/components/common/bottomnav/BottomNav.tsx
+++ src/components/common/bottomnav/BottomNav.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 export default function BottomNav() {
   const pathname = usePathname();
-  const [activeMenu, setActiveMenu] = useState<number>(1);
+  const [activeMenu, setActiveMenu] = useState<string>("/");
 
   const liStyle = "font-[600] flex justify-center px-[6px] w-[44px] text-[14px] whitespace-nowrap transition-colors";
 
@@ -24,12 +24,12 @@ export default function BottomNav() {
       <div className="bg-[#FAFAFF] pb-[42px] h-[93px] pt-[8px] w-full sticky bottom-0 shrink-0">
         <div className="flex justify-between items-center mx-[20px]">
           {bottomMenu.map((menu) => (
-            <div key={menu.id} onClick={() => setActiveMenu(menu.id)}>
+            <div key={menu.id} onClick={() => setActiveMenu(menu.link)}>
               <Link href={menu.link}>
                 <div className={liStyle}>
                   <div className="flex flex-col items-center gap-[4px] px-[6px]">
-                    <div className="p-[6px]">{activeMenu !== menu.id ? menu.icon : menu.blueIcon}</div>
-                    <p className={activeMenu !== menu.id ? "" : "text-primarystrong"}>{menu.label}</p>
+                    <div className="p-[6px]">{activeMenu !== menu.link ? menu.icon : menu.blueIcon}</div>
+                    <p className={activeMenu !== menu.link ? "" : "text-primarystrong"}>{menu.label}</p>
                   </div>
                 </div>
               </Link>

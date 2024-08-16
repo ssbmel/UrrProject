48608b345ef33cdb51962d3a67d5119eb90d19da
ssbmel <sssb2046@gmail.com>
2024-08-17 01:12:10 +0900

    fix: 리뷰 해당 상품상세페이지로 링크 연결

--- src/components/mypage/MyReviewList.tsx
+++ src/components/mypage/MyReviewList.tsx
@@ -8,6 +8,7 @@ import { Review } from "../../../types/common";
 import defaultImg from "../../../public/images/default.png";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const MyReviewList = () => {
   const [reviewData, setReviewData] = useState<Review[]>([]);
@@ -69,22 +70,26 @@ const MyReviewList = () => {
               <li key={item.id} className="pt-[18px] pb-[18px] text-[14px] border-b flex flex-col gap-[18px]">
                 <div className="flex justify-between items-center w-full">
                   <div className="flex items-center w-[calc(100%-58px)] gap-[12px]">
-                    <div className="relative w-[56px] h-[56px] rounded-[6px]">
-                      <Image
-                        src={
-                          (Array.isArray(item.review_images) ? item.review_images[0] : item.review_images) || defaultImg
-                        }
-                        alt="product_review_image"
-                        fill
-                        sizes="56px"
-                        priority
-                        className="bg-slate-300 rounded-[6px] object-cover"
-                      />
-                    </div>
-                    <div className="flex flex-col gap-[6px] w-[calc(100%-90px)]">
-                      <h3 className="font-[400] overflow-hidden text-ellipsis whitespace-nowrap">{item.title}</h3>
-                      <p className="text-[12px] text-[#B2B5B8]">{formatDate(item.created_at)}</p>
-                    </div>
+                    <Link href={`/products/detail/${item.product_id}`} className="flex items-center w-full gap-[12px]">
+                      <div className="relative w-[56px] h-[56px] rounded-[6px]">
+                        <Image
+                          src={
+                            (Array.isArray(item.review_images) ? item.review_images[0] : item.review_images) ||
+                            defaultImg
+                          }
+                          alt="product_review_image"
+                          fill
+                          sizes="56px"
+                          priority
+                          className="bg-slate-300 rounded-[6px] object-cover"
+                        />
+                      </div>
+                      <div className="flex flex-col gap-[6px] w-[calc(100%-90px)]">
+                        <h3 className="font-[400] overflow-hidden text-ellipsis whitespace-nowrap">{item.title}</h3>
+                        <p className="text-[12px] text-[#B2B5B8] truncate">{item.review_content}</p>
+                        <p className="text-[12px] text-[#B2B5B8]">{formatDate(item.created_at)}</p>
+                      </div>
+                    </Link>
                   </div>
                   <button onClick={() => handleDelete(item)} className="p-[9px]">
                     <TrashCan />

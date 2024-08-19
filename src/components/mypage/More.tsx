fe22f541fc5a795363afea67a63040859c7182be
LEE-NS <ogawa9341@gmail.com>
2024-08-19 20:02:57 +0900

    update : More component styled

--- src/components/mypage/More.tsx
+++ src/components/mypage/More.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+"use client";
 
-const More = () => {
+import React from "react";
+import XIcon from "../../../public/icon/XIcon.svg";
+
+interface Props {
+  isClicked: boolean;
+  setIsClicked: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const More = ({ isClicked, setIsClicked }: Props) => {
+  const closeWithBg = () => {
+    setIsClicked(false);
+  };
   return (
-    <div className="absolute top-0 left-0">
-      <div>더보기를 누르면 들어올 수 있음</div>
-      <section></section>
+    <div
+      className={`${isClicked ? "block" : "hidden"} fixed top-0 left-0 bg-[#3436378C] w-full h-full xl:shadow-2xl z-50`}
+    >
+      <div className="absolute xl:top-1/2 xl:left-1/2 w-full h-full xl:w-[700px] xl:h-[600px] xl:ml-[-350px] xl:mt-[-300px] bg-[#fffffe] xl:shadow-md xl:rounded-[24px] flex flex-col justify-between p-[24px]">
+        <div className="flex justify-between">
+          <h3 className="text-[20px] font-[700]">더보기(여기는 동적 타이틀이 들어온다)</h3>
+          <button onClick={closeWithBg}>
+            <XIcon />
+          </button>
+        </div>
+        <section className="xl:h-[500px]">여기는 추가로 로드되는 내용이 들어온다 useInfiniteQuery</section>
+      </div>
     </div>
   );
 };
--- src/components/mypage/Shopping.tsx
+++ src/components/mypage/Shopping.tsx
@@ -4,13 +4,7 @@ import MyOrderedList from "./MyOrderedList";
 const Shopping = () => {
   return (
     <div className="flex flex-col gap-[20px]">
-      <div className="flex flex-col gap-[20px] p-[20px] xl:gap-[37px]">
-        <div className="flex justify-between items-center">
-          <h2 className="text-[20px] font-bold">주문 내역</h2>
-          <span>더보기</span>
-        </div>
-        <MyOrderedList />
-      </div>
+      <MyOrderedList />
     </div>
   );
 };

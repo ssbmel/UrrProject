daff93dbaa416035edbeabc5d1996d8fee95ca53
ssbmel <sssb2046@gmail.com>
2024-10-02 16:29:31 +0900

    style: 메인페이지 배너 파일 교체

--- src/components/main/swiper/Banner.tsx
+++ src/components/main/swiper/Banner.tsx
@@ -7,17 +7,12 @@ import "swiper/css/pagination";
 import "./styles.css";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import Image from "next/image";
-import Link from "next/link";
 import banner1 from "../../../../public/bgImg/banner1.jpg";
 import banner2 from "../../../../public/bgImg/banner2.jpg";
 import banner3 from "../../../../public/bgImg/banner3.jpg";
-import banner4 from "../../../../public/bgImg/banner4.jpg";
-import banner5 from "../../../../public/bgImg/banner5.jpg";
 import minibanner1 from "../../../../public/bgImg/minibanner1.jpg";
 import minibanner2 from "../../../../public/bgImg/minibanner2.jpg";
 import minibanner3 from "../../../../public/bgImg/minibanner3.jpg";
-import minibanner4 from "../../../../public/bgImg/minibanner4.jpg";
-import minibanner5 from "../../../../public/bgImg/minibanner5.jpg";
 
 export default function Banner() {
   return (
@@ -38,7 +33,7 @@ export default function Banner() {
     >
       <SwiperSlide>
         <div className="xl:w-full xl:h-[900px] relative">
-          <Image src={banner1} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-contain" />
+          <Image src={banner1} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-cover" />
         </div>
         <div className="w-full h-[328px] relative xl:hidden">
           <Image src={minibanner1} alt="mainImg" fill sizes="375px 328px" className="object-contain" />
@@ -47,55 +42,24 @@ export default function Banner() {
 
       <SwiperSlide>
         <div className="xl:w-full relative">
-          <Link href={"/products/detail/a291eb38-cdd7-4a97-87d0-314cec358b1f"}>
             <div className="xl:w-full xl:h-[900px] relative">
               <Image src={banner2} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-contain" />
             </div>
-          </Link>
         </div>
 
         <div className="w-full relative xl:hidden">
-          <Link href={"/products/detail/a291eb38-cdd7-4a97-87d0-314cec358b1f"}>
             <div className="w-full h-[328px] relative xl:hidden">
               <Image src={minibanner2} alt="mainImg" fill sizes="375px 328px" className="object-contain" />
             </div>
-          </Link>
         </div>
       </SwiperSlide>
 
       <SwiperSlide>
         <div className="xl:w-full xl:h-[900px] relative">
-          <Image src={banner3} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-contain" />
+          <Image src={banner3} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-cover" />
         </div>
         <div className="w-full h-[328px] relative xl:hidden">
           <Image src={minibanner3} alt="mainImg" fill sizes="375px 328px" className="object-contain" />
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <div className="xl:w-full xl:h-[900px] relative">
-          <Image src={banner4} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-contain" />
-        </div>
-        <div className="w-full h-[328px] relative xl:hidden">
-          <Image src={minibanner4} alt="mainImg" fill sizes="375px 328px" className="object-contain" />
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <div className="xl:w-full relative">
-          <Link href={"/influencer"}>
-            <div className="xl:w-full xl:h-[900px] relative">
-              <Image src={banner5} alt="mainImg" fill sizes="(min-width:768px) 50vw 100vw" priority className="object-contain" />
-            </div>
-          </Link>
-        </div>
-
-        <div className="w-full relative xl:hidden">
-          <Link href={"/influencer"}>
-            <div className="w-full h-[328px] relative xl:hidden">
-              <Image src={minibanner5} alt="mainImg" fill sizes="375px 328px" className="object-contain" />
-            </div>
-          </Link>
         </div>
       </SwiperSlide>
     </Swiper>
